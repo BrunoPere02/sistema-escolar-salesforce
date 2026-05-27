73bb9fb9d798670e329fd2784860ee6218a822a4
BrunoPere02 <bs020323@gmail.com>
2026-05-27 11:10:38 -0300

    feat: botao novo registro no componente lista alunos

--- force-app/main/default/lwc/lista_alunos/lista_alunos.js
+++ force-app/main/default/lwc/lista_alunos/lista_alunos.js
@@ -1,4 +1,5 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 import getAlunos from '@salesforce/apex/AlunoController.getAlunos';
 
 const COLUMNS = [
@@ -8,17 +9,49 @@ const COLUMNS = [
     { label: 'Status', fieldName: 'Status__c' }
 ];
 
-export default class Lista_alunos extends LightningElement {
+export default class Lista_alunos extends NavigationMixin(LightningElement) {
     columns = COLUMNS;
-    alunos;
+    @track alunos;
     erro;
+    statusSelecionado = '';
+
+    statusOptions = [
+        { label: 'Todos', value: '' },
+        { label: 'Ativo', value: 'Ativo' },
+        { label: 'Inativo', value: 'Inativo' },
+        { label: 'Formado', value: 'Formado' }
+    ];
+
+    _todosAlunos = [];
 
     @wire(getAlunos)
     wiredAlunos({ data, error }) {
         if (data) {
+            this._todosAlunos = data;
             this.alunos = data;
         } else if (error) {
             this.erro = error;
         }
+    }
+
+    handleFiltro(event) {
+        this.statusSelecionado = event.detail.value;
+        if (this.statusSelecionado === '') {
+            this.alunos = this._todosAlunos;
+        } else {
+            this.alunos = this._todosAlunos.filter(
+                aluno => aluno.Status__c === this.statusSelecionado
+            );
+        }
+    }
+
+    novoRegistro() {
+        this[NavigationMixin.Navigate]({
+            type: 'standard__objectPage',
+            attributes: {
+                objectApiName: 'Aluno__c',
+                actionName: 'new'
+            }
+        });
     }
 }
